86ee04d5fd2ad9874038c583cf279df610728f33
harijoshi07 <hariojoshi@gmail.com>
2026-06-18 15:51:03 +0545

    feat(ui): connect the whole data flow to ui

--- app/src/main/java/com/example/newsfeed/MainActivity.java
+++ app/src/main/java/com/example/newsfeed/MainActivity.java
@@ -7,7 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.newsfeed.ui.headlines.HeadlinesAdapter;
+import com.example.newsfeed.ui.headlines.HeadlinesViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +28,13 @@ public class MainActivity extends AppCompatActivity {
             view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        HeadlinesAdapter headlinesAdapter = new HeadlinesAdapter();
+        headlinesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        headlinesRecyclerView.setAdapter(headlinesAdapter);
+
+        HeadlinesViewModel headlinesViewModel = new ViewModelProvider(this)
+                .get(HeadlinesViewModel.class);
+        headlinesViewModel.getArticles().observe(this, headlinesAdapter::setArticles);
     }
 }
